9fa074199e83fc855a9f94e7b23c00fee23b59d2
enoughkaran <146523541+enoughkaran@users.noreply.github.com>
2024-05-23 10:37:30 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC29Cbup6UiRUx9ChE5e3BAf3CsOdrCLww",
+    authDomain: "study-materials-2.firebaseapp.com",
+    databaseURL: "https://study-materials-2-default-rtdb.firebaseio.com",
+    projectId: "study-materials-2",
+    storageBucket: "study-materials-2.appspot.com",
+    messagingSenderId: "152838673427",
+    appId: "1:152838673427:web:fb887b6d6a10f50f38dbe6",
+    measurementId: "G-J15N482KK0"
+  };
